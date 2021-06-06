e02652e87717ad03df2b2e2ee509b052a6644164
rachelsch1 <schwarz.rachel.e@gmail.com>
2021-06-06 12:12:51 -0400

    initial solution

--- src/main/java/ex07/base/ex07.java
+++ src/main/java/ex07/base/ex07.java
@@ -1,4 +1,25 @@
 package ex07.base;
 
+import java.util.Scanner;
+
 public class ex07 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("What is the length of the room in feet? ");
+        String str1 = input.nextLine();
+        int length = Integer.parseInt(str1);
+
+        System.out.print("What is the width of the room in feet? ");
+        String str2 = input.nextLine();
+        int width = Integer.parseInt(str2);
+
+        System.out.println("You entered dimensions of " + length + " feet by " + width + " feet.");
+
+        int area = length * width;
+        final double convFact = 0.09290304;
+        double meters = area * convFact;
+
+        System.out.println("The area is\n" + area + " square feet\n" + meters + " square meters.");
+    }
 }
